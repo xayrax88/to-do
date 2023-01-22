57fa57f3e1f2b26bb784869e43d6e5719056e23d
Anouxay Sichanh <ainthaly@gmail.com>
2023-01-22 10:34:53 -0500

    Added Todo fn, new Todo, remove Todo & get Todo fn

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ $(document).ready(function () {
         this.task = task;
     }
 
+    //Add new Todo
+    function addNewTodo(task) {
+        var t = new Todo(task);
+        todos.push(t);
+        saveTodos();
+    }
 
+    //Remove Todo
+    function removeTodo(index) {
+        todos.splice(index, 1);
+        saveTodos();
+    }
 
-
-
-
-
-
-
-
+    //Get Todo
+    function getTodo(index) {
+        return todos[index];
+    }
 
 
 
