67959ada8029310ddb5aaa9b578f4e41f6274c11
Anouxay Sichanh <ainthaly@gmail.com>
2023-01-22 11:02:42 -0500

    localStorage fn & todo input

--- app.js
+++ app.js
@@ -39,8 +39,34 @@ $(document).ready(function () {
         }
     }
 
+    //Initialize todo app
+    getTodos();
+    listTodos();
 
 
+    //List Todos
+    function listTodos() {
+        var html = "";
+        for (var i in todos) {
+            //console.log(todos[i].name);
+            var todo = todos[i];
+            var task = todo.task;
+            //            var completed = todo.completed;
+            //            html += "<li>" + task + " <span>" + completed + "</span></li>";
+            html += "<li>" + task + "</li>";
+        }
+        $("#list-todos").html(html);
+    }
+
+
+    $("#add-todo-form").submit(function (e) {
+        e.preventDefault(); // Prevents the default behavior!
+        var name = $("#todo-task").val();
+        // validate...
+        addNewTodo(name);
+        //        addNewTodo(task);
+        listTodos();
+    });
 
 
 
